97c0fdf4fb0ece8f6de17456f8dafd74f0552e45
Alex Baskakau <lenovos6000v4@gmail.com>
2023-07-25 14:14:44 +0100

    Задание сделано

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+	if (d < 0) {
+		arr = [];
+	} else if (d > 0) {
+		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+	} else {
+		arr[0] = -b / (2 * a);
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let p = percent / 100 / 12;
+	let s = amount - contribution;
+	monthPay = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+	totalMortgage = (monthPay * countMonths).toFixed(2);
+	return parseFloat(totalMortgage);
+
 }
