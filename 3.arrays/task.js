4169afb8ce8acdef2af28a380660989012c604c0
Alex Baskakau <lenovos6000v4@gmail.com>
2023-08-07 13:28:38 +0100

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
+"use strict"
+
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length === arr2.length) {
+		return arr1.every((value, index) => {
+			return value === arr2[index];
+		});
+	}
+	return false;
 }
 
+
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+	const filteredOnGender = users.filter(user => user.gender === gender);
+	if (filteredOnGender.length === 0) {
+		return 0;
+	}
+	let ages = filteredOnGender.reduce((acc, user) => acc + user.age, 0);
+	let averageAge = ages / filteredOnGender.length;
+	return averageAge;
 }
